5ec6db2dc9c22febf356928b329b59f1b735998c
BhumipatSaengduan <152416744+BhumipatSaengduan@users.noreply.github.com>
2024-11-23 22:32:29 +0700

    Add files via upload

---
+++ lab01_2.java
@@ -0,0 +1,245 @@
+public class lab01_2 {
+    public static void main(String[] args) {
+        Turtle bob = new Turtle();
+        
+        //bob gonna draw Wat Phra That Doi Duthep for CMU Trekking 67
+        bob.bgcolor("gold");  
+        bob.penColor("black");
+        bob.width(5);
+        bob.speed(10);
+        
+        // first base;
+        bob.up();
+        bob.left(180);
+        bob.forward(200);
+        bob.down();
+        
+        bob.right(180);
+        bob.forward(700);
+        
+        bob.left(90);
+        bob.forward(20);
+        
+        bob.left(90);
+        bob.forward(700);
+        
+        bob.left(90);
+        bob.forward(20);
+        
+        bob.home();
+        bob.left(90);
+        bob.forward(20);
+
+        //something likes umbrella
+        //step1
+        bob.left(90);
+        bob.forward(50);
+        
+        bob.right(90);
+        bob.forward(20);
+        
+        bob.left(90);
+        bob.forward(100);
+        
+        bob.left(90);
+        bob.forward(20);
+        
+        //step 2
+        bob.left(180);
+        bob.forward(20);
+        
+        bob.right(90);
+        bob.forward(35);
+
+        bob.left(90);
+        bob.forward(300);
+
+        bob.right(90);
+        bob.forward(30);
+
+        bob.right(90);
+        bob.forward(300);
+
+        //step3
+        bob.left(180);
+        bob.forward(300);
+
+        bob.right(90);
+        bob.forward(75);
+
+        bob.left(90);
+        bob.forward(10);
+
+        bob.left(90);
+        bob.forward(180);
+
+        bob.left(90);
+        bob.forward(10);
+    
+        bob.left(90);
+        bob.forward(180);
+
+        bob.left(90);
+        bob.forward(20);
+
+        bob.left(90);
+        bob.forward(180);
+
+        bob.left(90);
+        bob.forward(10);
+        //
+        bob.left(90);
+        bob.forward(30);
+
+        bob.right(90);
+        bob.forward(10);
+
+        bob.left(90);
+        bob.forward(30);
+
+        bob.left(90);
+        bob.forward(10);
+        //
+        bob.right(90);
+        bob.forward(30);
+
+        bob.right(90);
+        bob.forward(10);
+
+        bob.left(90);
+        bob.forward(30);
+
+        bob.left(90);
+        bob.forward(10);
+        //
+        bob.right(90);
+        bob.forward(30);
+
+        bob.right(90);
+        bob.forward(10);
+
+        bob.left(90);
+        bob.forward(30);
+
+        bob.left(90);
+        bob.forward(20);
+
+        //step4 
+        bob.left(60);
+        bob.forward(104);
+
+        bob.left(60);
+        bob.forward(103.9);
+
+
+        //JD
+        bob.up();
+        bob.home();
+        bob.left(90);
+        bob.forward(20);
+        bob.right(90);
+        bob.forward(50);
+        bob.down();
+        
+        bob.left(90);
+        bob.forward(20);
+        
+        bob.right(90);
+        bob.forward(400);
+        
+        bob.right(90);
+        bob.forward(20);
+        
+        bob.right(180);
+        bob.forward(20);
+
+        bob.left(90);
+        bob.forward(50);
+        
+        bob.right(90);
+        bob.forward(20);
+
+        bob.left(90);
+        bob.forward(300);
+
+        bob.left(90);
+        bob.forward(20);
+        
+        bob.left(180);
+        bob.forward(20);
+
+        bob.right(60);
+        bob.forward(43.3);
+
+        bob.left(60);
+        bob.forward(50);
+        
+        bob.left(60);
+        bob.forward(43.3);
+    
+        bob.right(150);
+        bob.forward(300);
+
+        bob.right(150);
+        bob.forward(43.3);
+
+        bob.left(60);
+        bob.forward(50);
+
+        bob.left(60);
+        bob.forward(43.3);
+    
+        bob.up();
+        bob.left(120);
+        bob.forward(95);
+        bob.left(90);
+        bob.forward(25);
+        bob.down();
+
+        bob.right(75);
+        bob.forward(207);
+        
+        bob.left(75);
+        bob.forward(139.4);
+
+        bob.left(75);
+        bob.forward(207);
+
+        bob.left(180);
+        bob.forward(207);
+    
+        bob.left(15);
+        bob.right(45);
+        bob.forward(50);
+        
+        bob.right(45);
+        bob.forward(68.4);
+
+        bob.right(45);
+        bob.forward(50);
+
+        bob.right(180);
+        bob.forward(50);
+
+        bob.right(45);
+        bob.forward(20);
+
+        bob.left(90);
+        bob.forward(68.4);
+
+        bob.left(90);
+        bob.forward(20);
+
+        bob.left(180);
+        bob.forward(20);
+
+        bob.right(15);
+        bob.forward(132.1);
+
+        bob.right(150);
+        bob.forward(132.1);
+
+        bob.up();
+        bob.home();
+    }
+}
